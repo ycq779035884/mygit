5efdb4d0680e56cad7ae23efa19369fb520eef7f
杨乘权 <779035884@qq.com>
2021-01-20 14:13:38 +0800

    demo01

---
+++ demo01/Demo01.java
@@ -0,0 +1,11 @@
+public class Demo01{
+	public static void main(String args[]){
+		for(int i=1;i<=9;i++){
+			for(int j=1;j<=9;j++){
+				System.out.print(j+"*"+i+"="i*j);
+			}
+			System.out.println();
+		}
+	}
+	
+}
